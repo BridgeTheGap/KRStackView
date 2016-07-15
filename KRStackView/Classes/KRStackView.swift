f09a31666616f4605e038821419960221c1df42a
Joshua Park <joshuaparkwm@gmail.com>
2016-07-15 09:34:28 +0900

    + 0.2.0: Support for auto layout

--- KRStackView/Classes/KRStackView.swift
+++ KRStackView/Classes/KRStackView.swift
@@ -43,86 +43,85 @@ public class KRStackView: UIView {
     @IBInspectable public var shouldWrap: Bool = false
     
     public override func layoutSubviews() {
-        super.layoutSubviews()
-        
         guard enabled else { return }
         guard subviews.count > 0 else { return }
         
         let isVertical = direction == .Vertical
-        if translatesAutoresizingMaskIntoConstraints {  // Auto resizing
-            var endX: CGFloat!
-            var endY: CGFloat!
-            
-            if isVertical {
-                var maxWidth = subviews[0].frame.width + (itemOffset?[0] ?? 0.0)
-                for (i, view) in subviews.enumerate() {
-                    if maxWidth < view.frame.width + (itemOffset?[i] ?? 0.0) {
-                        maxWidth = view.frame.width + (itemOffset?[i] ?? 0.0)
-                    }
-                }
-                let maxX = insets.left + maxWidth + insets.right
-                
-                endX = shouldWrap ? maxX : max(maxX, frame.width)
-                endY = 0.0
-            } else {
-                endX = 0.0
-                
-                var maxHeight = subviews[0].frame.height + (itemOffset?[0] ?? 0.0)
-                for (i, view) in subviews.enumerate() {
-                    if maxHeight < view.frame.height + (itemOffset?[i] ?? 0.0) {
-                        maxHeight = view.frame.height + (itemOffset?[i] ?? 0.0)
-                    }
-                }
-                let maxY = insets.top + maxHeight + insets.bottom
-                
-                endY = shouldWrap ? maxY : max(maxY, frame.height)
-            }
-            
-            let useItemSpacing = itemSpacing?.count >= subviews.count - 1
-            let useItemOffset = itemOffset?.count >= subviews.count
-            
+
+        NSLayoutConstraint.deactivateConstraints(constraints + superview!.constraints.filter{ $0.firstItem === self || $0.secondItem === self })
+        translatesAutoresizingMaskIntoConstraints = true
+        for v in subviews { v.translatesAutoresizingMaskIntoConstraints = true }
+        
+        var endX: CGFloat!
+        var endY: CGFloat!
+        
+        if isVertical {
+            var maxWidth = subviews[0].frame.width + (itemOffset?[0] ?? 0.0)
             for (i, view) in subviews.enumerate() {
-                if isVertical {
-                    view.frame.origin.y = i == 0 ? insets.top : useItemSpacing ? endY + itemSpacing![i-1] : endY + spacing
-                    endY = view.frame.endPoint.y
-                } else {
-                    view.frame.origin.x = i == 0 ? insets.left : useItemSpacing ? endX + itemSpacing![i-1] : endX + spacing
-                    endX = view.frame.endPoint.x
-                }
-                
-                switch alignment {
-                case .Origin:
-                    if isVertical {
-                        view.frame.origin.x = useItemOffset ? insets.left + itemOffset![i] : insets.left
-                    } else {
-                        view.frame.origin.y = useItemOffset ? insets.top + itemOffset![i] : insets.top
-                    }
-                case .Center:
-                    if isVertical {
-                        view.center.x = useItemOffset ? round(endX/2.0) + itemOffset![i]/2.0 : round(endX/2.0)
-                    } else {
-                        view.center.y = useItemOffset ? round(endY/2.0) + itemOffset![i]/2.0 : round(endY/2.0)
-                    }
-                case .EndPoint:
-                    if isVertical {
-                        view.frame.origin.x = endX - (insets.right+view.frame.width)
-                        if useItemOffset { view.frame.origin.x -= itemOffset![i] }
-                    } else {
-                        view.frame.origin.y = endY - (insets.bottom+view.frame.height)
-                        if useItemOffset { view.frame.origin.y -= itemOffset![i] }
-                    }
+                if maxWidth < view.frame.width + (itemOffset?[i] ?? 0.0) {
+                    maxWidth = view.frame.width + (itemOffset?[i] ?? 0.0)
                 }
             }
+            let maxX = insets.left + maxWidth + insets.right
             
+            endX = shouldWrap ? maxX : max(maxX, frame.width)
+            endY = 0.0
+        } else {
+            endX = 0.0
+            
+            var maxHeight = subviews[0].frame.height + (itemOffset?[0] ?? 0.0)
+            for (i, view) in subviews.enumerate() {
+                if maxHeight < view.frame.height + (itemOffset?[i] ?? 0.0) {
+                    maxHeight = view.frame.height + (itemOffset?[i] ?? 0.0)
+                }
+            }
+            let maxY = insets.top + maxHeight + insets.bottom
+            
+            endY = shouldWrap ? maxY : max(maxY, frame.height)
+        }
+        
+        let useItemSpacing = itemSpacing?.count >= subviews.count - 1
+        let useItemOffset = itemOffset?.count >= subviews.count
+        
+        for (i, view) in subviews.enumerate() {
             if isVertical {
-                frame.size.width = endX
-                frame.size.height = shouldWrap ? endY + insets.bottom : max(endY + insets.bottom, frame.height)
+                view.frame.origin.y = i == 0 ? insets.top : useItemSpacing ? endY + itemSpacing![i-1] : endY + spacing
+                endY = view.frame.endPoint.y
             } else {
-                frame.size.width = shouldWrap ? endX + insets.right : max(endX + insets.right, frame.width)
-                frame.size.height = endY
+                view.frame.origin.x = i == 0 ? insets.left : useItemSpacing ? endX + itemSpacing![i-1] : endX + spacing
+                endX = view.frame.endPoint.x
             }
-        } else {    // Auto layout
             
+            switch alignment {
+            case .Origin:
+                if isVertical {
+                    view.frame.origin.x = useItemOffset ? insets.left + itemOffset![i] : insets.left
+                } else {
+                    view.frame.origin.y = useItemOffset ? insets.top + itemOffset![i] : insets.top
+                }
+            case .Center:
+                if isVertical {
+                    view.center.x = useItemOffset ? round(endX/2.0) + itemOffset![i]/2.0 : round(endX/2.0)
+                } else {
+                    view.center.y = useItemOffset ? round(endY/2.0) + itemOffset![i]/2.0 : round(endY/2.0)
+                }
+            case .EndPoint:
+                if isVertical {
+                    view.frame.origin.x = endX - (insets.right+view.frame.width)
+                    if useItemOffset { view.frame.origin.x -= itemOffset![i] }
+                } else {
+                    view.frame.origin.y = endY - (insets.bottom+view.frame.height)
+                    if useItemOffset { view.frame.origin.y -= itemOffset![i] }
+                }
+            }
+        }
+        
+        if isVertical {
+            frame.size.width = endX
+            frame.size.height = shouldWrap ? endY + insets.bottom : max(endY + insets.bottom, frame.height)
+        } else {
+            frame.size.width = shouldWrap ? endX + insets.right : max(endX + insets.right, frame.width)
+            frame.size.height = endY
         }
     }
 }
